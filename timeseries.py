530042993fb679036d8108365e99911c35a5e5ff
sq5rix <sq5rix@gmail.com>
2019-08-19 23:14:14 +0200

    grangers_causation_matrix

--- timeseries.py
+++ timeseries.py
@@ -6,6 +6,7 @@ from statsmodels.tsa.stattools import kpss
 from statsmodels.tsa.seasonal import seasonal_decompose
 from statsmodels.tsa.stattools import acf, pacf
 from statsmodels.tsa.arima_model import ARIMA
+from statsmodels.tsa.stattools import grangercausalitytests
 
 class StationarityTests:
     def __init__(self, significance=.05):
@@ -103,3 +104,29 @@ def arima(ts_log):
     plt.plot(results_ARIMA.fittedvalues, color='red')
     plt.title('RSS: %.4f'% sum((results_ARIMA.fittedvalues-ts_log_diff)**2))
     
+
+maxlag=12
+test = 'ssr_chi2test'
+def grangers_causation_matrix(data, variables, test='ssr_chi2test', verbose=False):    
+    """Check Granger Causality of all possible combinations of the Time series.
+    The rows are the response variable, columns are predictors. The values in the table 
+    are the P-Values. P-Values lesser than the significance level (0.05), implies 
+    the Null Hypothesis that the coefficients of the corresponding past values is 
+    zero, that is, the X does not cause Y can be rejected.
+
+    data      : pandas dataframe containing the time series variables
+    variables : list containing names of the time series variables.
+    """
+    df = pd.DataFrame(np.zeros((len(variables), len(variables))), columns=variables, index=variables)
+    for c in df.columns:
+        for r in df.index:
+            test_result = grangercausalitytests(data[[r, c]], maxlag=maxlag, verbose=False)
+            p_values = [round(test_result[i+1][0][test][1],4) for i in range(maxlag)]
+            if verbose: print(f'Y = {r}, X = {c}, P Values = {p_values}')
+            min_p_value = np.min(p_values)
+            df.loc[r, c] = min_p_value
+    df.columns = [var + '_x' for var in variables]
+    df.index = [var + '_y' for var in variables]
+    return df
+
+#grangers_causation_matrix(df, variables = df.columns)        
