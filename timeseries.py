290cfb1082070db62d1c1bf5e0d76fcec4abdb3e
sq5rix <sq5rix@gmail.com>
2019-08-19 23:15:39 +0200

    cointegration_test

--- timeseries.py
+++ timeseries.py
@@ -7,6 +7,7 @@ from statsmodels.tsa.seasonal import seasonal_decompose
 from statsmodels.tsa.stattools import acf, pacf
 from statsmodels.tsa.arima_model import ARIMA
 from statsmodels.tsa.stattools import grangercausalitytests
+from statsmodels.tsa.vector_ar.vecm import coint_johansen
 
 class StationarityTests:
     def __init__(self, significance=.05):
@@ -130,3 +131,20 @@ def grangers_causation_matrix(data, variables, test='ssr_chi2test', verbose=Fals
     return df
 
 #grangers_causation_matrix(df, variables = df.columns)        
+
+
+
+def cointegration_test(df, alpha=0.05):
+    """Perform Johanson's Cointegration Test and Report Summary"""
+    out = coint_johansen(df,-1,5)
+    d = {'0.90':0, '0.95':1, '0.99':2}
+    traces = out.lr1
+    cvts = out.cvt[:, d[str(1-alpha)]]
+    def adjust(val, length= 6): return str(val).ljust(length)
+
+    # Summary
+    print('Name   ::  Test Stat > C(95%)    =>   Signif  \n', '--'*20)
+    for col, trace, cvt in zip(df.columns, traces, cvts):
+        print(adjust(col), ':: ', adjust(round(trace,2), 9), ">", adjust(cvt, 8), ' =>  ' , trace > cvt)
+
+#cointegration_test(df)
